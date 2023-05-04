9632c4df2756a302a9337fa360db3bf02b76103f
Vitor Bulbovas <vitorbulbovas1@hotmail.com>
2023-05-04 15:13:01 -0300

    :tada: commit inicial

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <p className="title">Teste</p>
+      <p className="subtitle">Teste</p>
+      <p className="text">Teste</p>
+    </>
+  );
+}
+
+export default App;
