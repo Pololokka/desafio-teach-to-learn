2bdc54b8865c807b408123a5a001d6c20a8889d5
Vitor Bulbovas <vitorbulbovas1@hotmail.com>
2023-05-08 16:26:47 -0300

    :sparkles: feat: feita página de pintura

--- src/Pages/Paint/Index.jsx
+++ src/Pages/Paint/Index.jsx
@@ -1,6 +1,10 @@
 import React from "react";
+import fundoPaint from "../../assets/img/fundo-paint.png";
+
+import { textPagePaint } from "../../Data/text";
 
 import { Link } from "react-router-dom";
+import TextSection from "../../Components/Class/TextSection/Index";
 
 class Paint extends React.Component {
   render() {
@@ -25,6 +29,32 @@ class Paint extends React.Component {
             </ul>
           </nav>
         </header>
+
+        <main className="main__container">
+          <img src={fundoPaint} className="bg-img" />
+
+          <iframe
+            width="50%"
+            height="420px"
+            src="https://www.youtube.com/embed/4KNkOPTa_po"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowfullscreen
+          ></iframe>
+
+          <iframe
+            width="50%"
+            height="420px"
+            src="https://www.youtube.com/embed/hLRSCV6N1IU"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowfullscreen
+          ></iframe>
+
+          <TextSection sectionTitle="Sobre" sectionText={textPagePaint} />
+        </main>
       </>
     );
   }
